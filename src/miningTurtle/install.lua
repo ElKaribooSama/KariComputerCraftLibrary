51464cd07258428539b45f9e7db6946e0841f64b
ElKariboosama <tsunaris123456789@gmail.com>
2025-02-01 18:42:12 +0100

    miningTurtle installer

---
+++ src/miningTurtle/install.lua
@@ -0,0 +1,14 @@
+local libraries = {
+    "https://raw.githubusercontent.com/ElKaribooSama/KariComputerCraftLibrary/refs/heads/main/lib/chestInteraction.lua",
+    "https://raw.githubusercontent.com/ElKaribooSama/KariComputerCraftLibrary/refs/heads/main/lib/oreNode.lua",
+    "https://raw.githubusercontent.com/ElKaribooSama/KariComputerCraftLibrary/refs/heads/main/lib/movement.lua",
+}
+
+local sources = {
+    "https://raw.githubusercontent.com/ElKaribooSama/KariComputerCraftLibrary/refs/heads/main/src/miningTurtle.lua"
+}
+
+return {
+    lib = libraries,
+    src = sources,
+}
